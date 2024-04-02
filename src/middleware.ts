bf95e210acd75e2c8c21b62c5c40e3af2832604d
toni-krstic <krstic.toni.tk@gmail.com>
2024-04-02 14:06:10 +0200

    increase rate limit

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,7 +12,7 @@ const rateLimitMap: RateLimitMap = new Map();
 export default authMiddleware({
   beforeAuth: (req) => {
     const ip = req.headers.get("x-forwarded-for") ?? req.ip;
-    const limit = 10; // Limiting requests to 10 per minute per IP
+    const limit = 20; // Limiting requests to 20 per minute per IP
     const windowMs = 60 * 1000; // 1 minute
 
     if (!rateLimitMap.has(ip)) {
